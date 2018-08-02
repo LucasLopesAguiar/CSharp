5d18af1f3d6a98c0174ebd4c768e4834464456a3
Lucas De Aguiar <lucaslopesdeaguiar@outlook.com>
2018-08-02 20:37:27 -0300

    Orientção Objeto/Herança

---
+++ Aulas00/0208_Console_OrientadoOBJ/Modelos/Carro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0208_Console_OrientadoOBJ.Modelos
+{
+    class Carro : Veiculo
+    {     
+           
+    
+    
+    }
+}
---
+++ Aulas00/0208_Console_OrientadoOBJ/Modelos/Veiculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0208_Console_OrientadoOBJ.Modelos
+{
+    class Veiculo
+    {
+        public String Placa { get; set; }
+        public String Modelo { get; set; }
+        public int Ano { get; set; }
+        
+
+    }
+}
