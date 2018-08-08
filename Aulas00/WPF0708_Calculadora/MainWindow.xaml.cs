02237ce61b498b85ed206ce24748b7a62a1e7cfb
Lucas De Aguiar <lucaslopesdeaguiar@outlook.com>
2018-08-08 17:16:14 -0300

    Calculadora quase concluida, falta ajuste no resultado e na potencia.

--- Aulas00/WPF0708_Calculadora/MainWindow.xaml.cs
+++ Aulas00/WPF0708_Calculadora/MainWindow.xaml.cs
@@ -22,23 +22,174 @@ namespace WPF0708_Calculadora
     {
         //Toda vez que selecionar algo, vai validar se é operador para limpar a tela
         bool clica_operacao = false;
+        bool mostra_calculo = false;
+        double num1 = 0;
+        double calculo = 0;
+        string qual_operacao= "";
+
 
         //Função que válida se foi digitado numero ou operador
-        public void ler_num(int Num_Lido)
+        public void ler_num(double Num_Lido)
         {
             if (clica_operacao == false)
 
                 Visor.Text += Num_Lido;
 
-            else Visor.Text = Num_Lido.ToString(); 
+            else Visor.Text = Num_Lido.ToString();
+
+            //Depois de uma operação ele valida e deixa aparecer mais numeros
+            clica_operacao = false;
 
 
         }
+
+        //Limpar a tela apos escolher operação
+        public void Botao_Operacao (string Qual_operacao)
+        {
+
+            clica_operacao = true;
+            if (mostra_calculo == true)
+                Calcula_Apresenta();
+
+            //Determino que aqui irá puxar o operar definido em cada click 
+            this.qual_operacao = Qual_operacao;
+            num1 = double.Parse(Visor.Text);
+            mostra_calculo = true;
+        }
         
+        public void Calcula_Apresenta()
+        {
+            if (qual_operacao == "+")
+                calculo = num1 + double.Parse(Visor.Text);
+            if (qual_operacao == "-")
+                calculo = num1 - double.Parse(Visor.Text);
+            if (qual_operacao == "/")
+                calculo = num1 / double.Parse(Visor.Text);
+            if (qual_operacao == "*")
+                calculo = num1 * double.Parse(Visor.Text);
+            if (qual_operacao == "√")
+                calculo = Math.Sqrt(double.Parse(Visor.Text));
+            //if (qual_operacao == "%")
+            //calculo = num1 % double.Parse(Visor.Text);
+
+            Visor.Text = calculo.ToString();
+
+        }
 
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Numero0_Click(object sender, RoutedEventArgs e)
+        {
+            ler_num(0);
+        }
+
+        private void Numero1_Click(object sender, RoutedEventArgs e)
+        {
+            ler_num(1);
+        }
+
+        private void Numero2_Click(object sender, RoutedEventArgs e)
+        {
+            ler_num(2);
+        }
+
+        private void Numero3_Click(object sender, RoutedEventArgs e)
+        {
+            ler_num(3);
+        }
+
+        private void Numero4_Click(object sender, RoutedEventArgs e)
+        {
+            ler_num(4);
+        }
+
+        private void Numero5_Click(object sender, RoutedEventArgs e)
+        {
+            ler_num(5);
+        }
+
+        private void Numero6_Click(object sender, RoutedEventArgs e)
+        {
+            ler_num(6);
+        }
+
+        private void Numero7_Click(object sender, RoutedEventArgs e)
+        {
+            ler_num(7);
+        }
+
+        private void Numero8_Click(object sender, RoutedEventArgs e)
+        {
+            ler_num(8);
+        }
+
+        private void Numero9_Click(object sender, RoutedEventArgs e)
+        {
+            ler_num(9);
+        }
+
+        private void Soma_Click(object sender, RoutedEventArgs e)
+        {
+            Botao_Operacao("+");
+        }
+
+        private void Subtrair_Click(object sender, RoutedEventArgs e)
+        {
+            Botao_Operacao("-");
+        }
+
+        private void Divisão_Click(object sender, RoutedEventArgs e)
+        {
+            Botao_Operacao("/");
+        }
+
+        private void Multiplicação_Click(object sender, RoutedEventArgs e)
+        {
+            Botao_Operacao("*");
+        }
+
+        private void RaizQuadrada_Click(object sender, RoutedEventArgs e)
+        {
+            Botao_Operacao("√"); 
+        }
+
+        private void Potencia_Click(object sender, RoutedEventArgs e)
+        {
+            Botao_Operacao("%");
+        }
+
+        private void ApagaDig_Click(object sender, RoutedEventArgs e)
+        {
+            int tamanho = Visor.Text.Length;
+            if (tamanho > 0)
+                Visor.Text = Visor.Text.Substring(0, tamanho - 1);
+        }
+
+        private void LimpaCampo_Click(object sender, RoutedEventArgs e)
+        {
+            Visor.Text = "";
+        }
+
+        private void Pontual_Click(object sender, RoutedEventArgs e)
+        {
+            double virg;
+            virg = Visor.Text.IndexOf(",");   //Procura por todo visor o caracter "," e se achar
+            if (virg >= 0)                    //retorna a void, ou seja, faz nada
+                return;
+            else                           //se não achar, a vírgula é inserida
+                Visor.Text += ",";
+
+         
+        }
+
+        private void Resultado_Click(object sender, RoutedEventArgs e)
+        {
+            Calcula_Apresenta();
+            mostra_calculo = false;
+                
         }
     }
 }
